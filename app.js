04f48e4293661dd561ee3236fd581c444965b9bc
harshitadatra <57798468+harshitadatra@users.noreply.github.com>
2020-12-24 12:06:11 +0530

    input output processing done

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-alert("this scripts works")
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");	
+
+var outputDiv = document.querySelector("#output");
+
+
+
+
+
+	function clickHandler() {
+    	   outputDiv.innerText = "ajsjsjsjsjsjsj  " + txtInput.value;
+    
+	};
+btnTranslate.addEventListener("click", clickHandler)
